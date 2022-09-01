e1ee649580005173a9ca067d60a4a3f12c5f3f83
hanzcrs <hanz.caroca.valenzuela@ciisa.cl>
2022-09-01 16:29:52 -0400

    creacion de juego adivinanza

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
+let numeroMaquina = Math.floor(Math.random() * (10-1)+1);
+//mostrar el numero en consola solo para revisar si salen numeros aleatorios
+//console.log(numeroMaquina);
 
+
+let vidas = 3;
+let numeroUsuario = parseInt(prompt('ADIVINE EL NUMERO DEL 1 AL 10'));
+while(numeroMaquina !== numeroUsuario && vidas >1){
+
+    console.log('Te equivocaste')
+    numeroMaquina = parseInt(prompt('ADIVINE EL NUMERO DEL 1 AL 10'));
+    vidas --
+}
+if (numeroMaquina === numeroUsuario){
+ // iconos con windows+coma tecla coma con dos puntos
+    console.log("Ganaste 😍");
+} else{
+    console.log("Perdiste 😂😢");
+}
 
 
 
