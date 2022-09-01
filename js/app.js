cb6aa39d1cb01a96356d562ec2270b81ef8db0a2
hanzcrs <hanz.caroca.valenzuela@ciisa.cl>
2022-09-01 16:41:55 -0400

    mejora de juego adivinanza

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 let numeroMaquina = Math.floor(Math.random() * (10-1)+1);
 //mostrar el numero en consola solo para revisar si salen numeros aleatorios
 //console.log(numeroMaquina);
-
-
 let vidas = 3;
 let numeroUsuario = parseInt(prompt('ADIVINE EL NUMERO DEL 1 AL 10'));
 while(numeroMaquina !== numeroUsuario && vidas >1){
-
-    console.log('Te equivocaste')
-    numeroMaquina = parseInt(prompt('ADIVINE EL NUMERO DEL 1 AL 10'));
+    let mensaje = 
+    (numeroMaquina > numeroUsuario) // ? true y : false
+    ? "El numero es mayor"
+    : "El numero Ingresado es menor"
+    console.log('Te equivocaste' + mensaje);
+    numeroUsuario = parseInt(prompt('ADIVINE EL NUMERO DEL 1 AL 10'));
     vidas --
 }
 if (numeroMaquina === numeroUsuario){
