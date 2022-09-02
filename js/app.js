b61cd13fdf29995282f28811ffa911d5d64dc1c5
hanzcrs <hanz.caroca.valenzuela@ciisa.cl>
2022-09-02 15:26:03 -0400

    creacionObjeto

--- js/app.js
+++ js/app.js
@@ -1,24 +1,20 @@
-let numeroMaquina = Math.floor(Math.random() * (10-1)+1);
-//mostrar el numero en consola solo para revisar si salen numeros aleatorios
-//console.log(numeroMaquina);
-let vidas = 3;
-let numeroUsuario = parseInt(prompt('ADIVINE EL NUMERO DEL 1 AL 10'));
-while(numeroMaquina !== numeroUsuario && vidas >1){
-    let mensaje = 
-    (numeroMaquina > numeroUsuario) // ? true y : false
-    ? "El numero es mayor"
-    : "El numero Ingresado es menor"
-    console.log('Te equivocaste' + mensaje);
-    numeroUsuario = parseInt(prompt('ADIVINE EL NUMERO DEL 1 AL 10'));
-    vidas --
-}
-if (numeroMaquina === numeroUsuario){
- // iconos con windows+coma tecla coma con dos puntos
-    console.log("Ganaste 😍");
-} else{
-    console.log("Perdiste 😂😢");
-}
+const computador = {
+    marca: "lenovo",
+    modelo: "legion gamer",
+    color: "negro",
+    precio: 790.990,
+    locales: ["paris", "falabella", "PC Express"],
+    estado: true,
+};
 
+//mostrar en consola solo marca
+console.log(computador.marca);
+//mostrar de otra forma
+console.log(computador["marca"]);
+//traer todo
+console.log(computador);
+// traer por locales en especifico
+console.log(computador.locales[6]);
 
 
 
